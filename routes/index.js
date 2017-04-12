832b1fa2ce9ba4949b21839d23a811694c454cd8
Charlie Crocker <cscrocker84@gmail.com>
2017-04-12 13:43:20 -0500

    added routes to index.js and navbar

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const router = Router();
 
 
 router.use(require('./loginRoute'));
-// router.use(require('./registerRoute'));
-// router.use(require('./logoutRoute'));
+router.use(require('./registerRoute'));
+router.use(require('./logoutRoute'));
 router.use(require('./likesRoute'));
 router.use(require('./homeRoute'));
 router.use(require('./profileRoute'));
