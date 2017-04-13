e7ee4872ce0a0b5bcb329e68ee5547e5739283f8
Phillip Lewis <phillip.lewisjr@gmail.com>
2017-04-13 09:10:05 -0500

    just some testing

--- controllers/homeCtrl.js
+++ controllers/homeCtrl.js
@@ -3,11 +3,21 @@
 
 const User = require("../models/users")
 
-// module.exports.render = (req, res) => {
-//   res.render("register", {page: "Register"});
-// }
+module.exports.render = (req, res) => {
+  res.render("register", {page: "Register"});
+}
 
-User.forge().fetchAll()
+// User.forge().fetchAll()
+// .then(models => {
+//   models.forEach(model => console.log(model.toJSON()))
+// })
+
+User.forge().query({where: {gender: "Female"}}).fetchAll()
+.then(models => {
+  models.forEach(model => console.log(model.toJSON()))
+})
+
+User.forge().query({where: {gender: "Male"}}).fetchAll()
 .then(models => {
   models.forEach(model => console.log(model.toJSON()))
 })
